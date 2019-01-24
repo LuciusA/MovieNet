7abb311e00a7bab315127baad8f9dc92da6fceb2
Antonin Lucius <lucius_a@etna-alternance.net>
2019-01-24 20:12:13 +0100

    Init Project

---
+++ MovieNetWPF/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MovieNetWPF
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Name = "Hello MVVM !";
+            MyCommand = new RelayCommand(MyCommandExecute);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand MyCommand { get; }
+
+        void MyCommandExecute()
+        {
+            Name = "Hello Click!";
+        }
+    }
+}
